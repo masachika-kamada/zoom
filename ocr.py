c7783ace1084d24c96ecc99bdc5c0905f0176f61
masachika <masachika.kmd@gmail.com>
2022-06-04 00:09:21 +0900

    [add] #6 OCRをするためのTesseractクラス

---
+++ ocr.py
@@ -0,0 +1,27 @@
+import pyocr
+import pyocr.builders
+from PIL import Image
+import re
+
+
+class Tesseract:
+    def __init__(self):
+        pyocr.tesseract.TESSERACT_CMD = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+        self.tool = pyocr.get_available_tools()[0]
+
+    def ocr(self, img_path):
+        """tesseractでOCR
+
+        Args:
+            img_path (str): 画像のパス
+        Returns:
+            result (int): OCR結果
+        """
+        img = Image.open(img_path)
+        result = self.tool.image_to_string(
+            img,
+            lang="eng",
+            builder=pyocr.builders.TextBuilder()
+        )
+        result = int(re.sub(r"\D", "", result))
+        return result
