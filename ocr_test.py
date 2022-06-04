d05a1cb270e9619730e8015617df64dfc48ccacc
masachika <masachika.kmd@gmail.com>
2022-06-04 12:59:58 +0900

    [update] #8 OCRの事前処理を関数化、(3)で3が出力されることを確認

--- ocr_test.py
+++ ocr_test.py
@@ -3,8 +3,20 @@ import pyocr
 import pyocr.builders
 from PIL import Image
 import re
-import numpy as np
 # from ocr import Tesseract
+
+
+def cv2pil(image):
+    ''' OpenCV型 -> PIL型 '''
+    new_image = image.copy()
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
+    new_image = Image.fromarray(new_image)
+    return new_image
 
 
 class Tesseract:
@@ -20,55 +32,43 @@ class Tesseract:
         Returns:
             result (int): OCR結果
         """
-        img = Image.open(img_path)
-        result = self.tool.image_to_string(
-            img,
-            lang="eng",
-            builder=pyocr.builders.TextBuilder(tesseract_layout=6)
-        )
-        print("text", result)
-        result = self.tool.image_to_string(
-            img,
-            lang="eng",
-            builder=pyocr.builders.DigitBuilder()
-        )
-        print("num", result)
-        print(re.sub(r"\D", "", result))
-        result = int(re.sub(r"\D", "", result))
-        return result
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
+        # img = cv2.resize(img, (0, 0), fx=2, fy=2)
+
+        cnts, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # margin = 3
+        margin = 1
+        xmin = min([cv2.boundingRect(cnt)[0] for cnt in cnts[1:]]) - margin
+        xmax = max([cv2.boundingRect(cnt)[0] + cv2.boundingRect(cnt)[2] for cnt in cnts[1:]]) + margin
+
+        # kakko_width = 12
+        kakko_width = 6
+        white = (255, 255, 255)
+        cv2.rectangle(img, (xmin, 0), (xmin + kakko_width, img.shape[0]), white, -1)
+        cv2.rectangle(img, (xmax - kakko_width, 0), (xmax, img.shape[0]), white, -1)
+        cv2.imshow("img", img)
+        cv2.waitKey(0)
+
+        # img = cv2pil(img)
+
+        try:
+            result = self.tool.image_to_string(
+                Image.fromarray(img),
+                lang="eng",
+                builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+            )
+            print("text", result)
+            return int(re.sub(r"\D", "", result))
+        except cv2.error:
+            print("cv2.error")
+            return 0
 
 
 def main():
-    # tesseract = Tesseract()
-    # res = tesseract.ocr("./joiners.png")
-    # print(res)
-    img = cv2.imread("./joiners.png", cv2.IMREAD_GRAYSCALE)
-    img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
-    img = cv2.resize(img, (0, 0), fx=2, fy=2)
-    cv2.imshow("img", img)
-    kernel = np.ones((3, 3), np.uint8)
-    erosion = cv2.erode(img, kernel, iterations=1)
-
-    cnts, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    margin = 3
-    xmin = min([cv2.boundingRect(cnt)[0] for cnt in cnts[1:]]) - margin
-    xmax = max([cv2.boundingRect(cnt)[0] + cv2.boundingRect(cnt)[2] for cnt in cnts[1:]]) + margin
-
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    cnts = cnts[1:]
-    print(cnts)
-    res = cv2.drawContours(img_rgb, cnts, -1, (0, 255, 0), 1)
-    kakko_width = 12
-    white = (255, 255, 255)
-    cv2.rectangle(res, (xmin, 0), (xmin + kakko_width, img.shape[0]), white, -1)
-    cv2.rectangle(res, (xmax - kakko_width, 0), (xmax, img.shape[0]), white, -1)
-    cv2.drawMarker(res, (xmin, 0), (0, 0, 255), cv2.MARKER_CROSS, 10, 2)
-    cv2.drawMarker(res, (xmax, 0), (0, 0, 255), cv2.MARKER_CROSS, 10, 2)
-
-    cv2.imshow("dilation", erosion)
-    cv2.imshow("res", res)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    tesseract = Tesseract()
+    res = tesseract.ocr("./joiners.png")
+    print(res)
 
 
 if __name__ == "__main__":
