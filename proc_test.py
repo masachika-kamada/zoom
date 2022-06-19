22b8b4eeea47ccfddf241d03d07a2880692f81e7
masachika <masachika.kmd@gmail.com>
2022-06-20 02:22:36 +0900

    [update] 起動しているZoomを閉じることができた

--- proc_test.py
+++ proc_test.py
@@ -1,11 +1,21 @@
 import subprocess
+import os
 
 
-cmd = ["tasklist"]
-proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-result = proc.communicate()
-print(type(result))
-print(len(result))
-for res in result[0].splitlines():
-    if "Zoom.exe" in res.decode('utf-8'):
-        print(res)
+def check_zoom():
+    cmd = ["tasklist"]
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    result = proc.communicate()
+    print(type(result))
+    print(len(result))
+    for res in result[0].splitlines():
+        if "Zoom.exe" in res.decode('utf-8'):
+            print(res)
+            os.system("taskkill /im Zoom.exe")
+            return
+
+
+if __name__ == "__main__":
+    target_exe = "Zoom.exe"
+    check_zoom()
+    check_zoom()
